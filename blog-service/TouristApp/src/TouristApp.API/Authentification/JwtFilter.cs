48d74a54c9b10935baa2fa8daebe60f18a0ea75d
borh77 <borisahrnjez@gmail.com>
2026-04-09 20:03:42 +0200

    feat: integrate authorization from stakeholders service

---
+++ blog-service/TouristApp/src/TouristApp.API/Authentification/JwtFilter.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace TouristApp.API.Authentification
+{
+    public class JwtAuthenticationFilter : IAsyncActionFilter
+    {
+        private readonly JwtService _jwtService;
+
+        public JwtAuthenticationFilter(JwtService jwtService)
+        {
+            _jwtService = jwtService;
+        }
+
+        public async Task OnActionExecutionAsync(
+            ActionExecutingContext context,
+            ActionExecutionDelegate next)
+        {
+            var token =
+                context.HttpContext.Request.Headers["Authorization"]
+                    .ToString()
+                    .Replace("Bearer ", "");
+
+            if (!_jwtService.IsTokenValid(token))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            await next();
+        }
+    }
+}
---
+++ blog-service/TouristApp/src/TouristApp.API/Authentification/JwtService.cs
@@ -0,0 +1,60 @@
+﻿using System.Security.Claims;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+
+
+namespace TouristApp.API.Authentification
+{
+    public class JwtService
+    {
+        private readonly string _secret;
+
+        public JwtService(IConfiguration config)
+        {
+            _secret = config["Jwt:Secret"];
+        }
+
+        public string ExtractUsername(string token)
+        {
+            return GetPrincipal(token).Identity.Name;
+        }
+
+        public string ExtractRole(string token)
+        {
+            return GetPrincipal(token)
+                .Claims
+                .First(x => x.Type == ClaimTypes.Role)
+                .Value;
+        }
+
+        public bool IsTokenValid(string token)
+        {
+            try
+            {
+                GetPrincipal(token);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private ClaimsPrincipal GetPrincipal(string token)
+        {
+            var handler = new JwtSecurityTokenHandler();
+
+            return handler.ValidateToken(token,
+                new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    IssuerSigningKey =
+                        new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(_secret))
+                },
+                out _);
+        }
+    }
+}
