7d23b9b47b1783a637087fdcde957bbcd03f371d
emilijasimic1510 <emilija1510.s@gmail.com>
2026-04-14 03:13:02 +0200

    feat: add update profile endpoint PUT /api/users/me

--- stakeholders-service/src/main/java/com/soa/stakeholders/service/UserService.java
+++ stakeholders-service/src/main/java/com/soa/stakeholders/service/UserService.java
@@ -6,7 +6,7 @@ import com.soa.stakeholders.exception.BadRequestException;
 import com.soa.stakeholders.repository.AppUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
+import com.soa.stakeholders.dto.UpdateProfileRequest;
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -16,6 +16,31 @@ public class UserService {
     public ProfileResponse getMyProfile(String username) {
         AppUser user = appUserRepository.findByUsername(username)
                 .orElseThrow(() -> new BadRequestException("User not found"));
+
+        return new ProfileResponse(
+                user.getUsername(),
+                user.getEmail(),
+                user.getRole(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getProfileImage(),
+                user.getBiography(),
+                user.getMotto()
+        );
+    }
+
+
+    public ProfileResponse updateMyProfile(String username, UpdateProfileRequest request) {
+        AppUser user = appUserRepository.findByUsername(username)
+                .orElseThrow(() -> new BadRequestException("User not found"));
+
+        if (request.getFirstName() != null) user.setFirstName(request.getFirstName());
+        if (request.getLastName() != null) user.setLastName(request.getLastName());
+        if (request.getProfileImage() != null) user.setProfileImage(request.getProfileImage());
+        if (request.getBiography() != null) user.setBiography(request.getBiography());
+        if (request.getMotto() != null) user.setMotto(request.getMotto());
+
+        appUserRepository.save(user);
 
         return new ProfileResponse(
                 user.getUsername(),
