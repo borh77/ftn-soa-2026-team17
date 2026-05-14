97ed7471998a3db401c930bb1172b92b34114650
borh77 <borisahrnjez@gmail.com>
2026-05-14 16:11:46 +0200

    fix: naming

--- tour-service/TouristApp/src/Modules/Tours/TouristApp.Tours.Tests/ToursTestFactory.cs
+++ tour-service/TouristApp/src/Modules/Tours/TouristApp.Tours.Tests/ToursTestFactory.cs
@@ -1,8 +1,9 @@
-using TouristApp.BuildingBlocks.Tests;
-using TouristApp.Tours.Infrastructure.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+using TouristApp.Tours.Infrastructure.Database;
 using TouristApp.BuildingBlocks.Tests;
+using TouristApp.BuildingBlocks.Infrastructure.Database; 
 
 namespace TouristApp.Tours.Tests;
 
@@ -14,7 +15,15 @@ public class ToursTestFactory : BaseTestFactory<ToursContext>
             d => d.ServiceType == typeof(DbContextOptions<ToursContext>));
         if (descriptor != null) services.Remove(descriptor);
 
-        services.AddDbContext<ToursContext>(SetupTestContext());
+        var connectionString = DbConnectionStringBuilder.Build("Tours");
+
+        var dataSourceBuilder = new NpgsqlDataSourceBuilder(connectionString);
+        dataSourceBuilder.EnableDynamicJson(); // jsonb!
+        var dataSource = dataSourceBuilder.Build();
+
+        services.AddDbContext<ToursContext>(options =>
+            options.UseNpgsql(dataSource, x =>
+                x.MigrationsHistoryTable("__EFMigrationsHistory", "Tours")));
 
         return services;
     }
