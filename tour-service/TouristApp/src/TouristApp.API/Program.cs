9f261568817ba1ee6fb4da57c8a0d81219cf5866
borh77 <borisahrnjez@gmail.com>
2026-05-15 13:04:45 +0200

    fix: ensure migration for tour service are created

--- tour-service/TouristApp/src/TouristApp.API/Program.cs
+++ tour-service/TouristApp/src/TouristApp.API/Program.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Text;
 using TouristApp.API.Authentification;
 using TouristApp.API.Middleware;
 using TouristApp.API.Startup;
+using TouristApp.Tours.Infrastructure.Database;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -46,6 +48,37 @@ builder.Services.AddAuthorization(options =>
 });
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ToursContext>();
+
+    const int maxRetries = 10;
+    var delay = TimeSpan.FromSeconds(5);
+
+    for (int attempt = 1; attempt <= maxRetries; attempt++)
+    {
+        try
+        {
+            Console.WriteLine($"Pokušaj inicijalizacije baze {attempt}/{maxRetries}...");
+            context.Database.EnsureCreated();
+            Console.WriteLine("Baza je uspešno inicijalizovana.");
+            break;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Inicijalizacija baze nije uspela u pokušaju {attempt}: {ex.Message}");
+
+            if (attempt == maxRetries)
+            {
+                throw;
+            }
+
+            Thread.Sleep(delay);
+        }
+    }
+}
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
